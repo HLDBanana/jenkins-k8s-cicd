8d80fd4b2d18277c48ce8f64c9244e6818e2921a
韩利东 <hanlidong@ysstech.com>
2020-11-11 17:48:29 +0800

    修改打包镜像的脚本

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 参数 ：tag版本
 TAG=$1
+cd /opt/workspace/k8s-jenkins/jenkins-k8s-cicd
 docker bulid -t 47.114.1.23/hld-k8s/eureka-server:$TAG .
 docker push 47.114.1.23/hld-k8s/eureka-server:$TAG
