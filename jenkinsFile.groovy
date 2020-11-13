78b3ba14f8d7aa268951ede3a19e8abd19a05bb2
韩利东 <hanlidong@ysstech.com>
2020-11-13 14:27:43 +0800

    部署yaml文件

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -9,19 +9,19 @@ def server_name = "eureka"
 
 
 def k8s_auth = "9914859b-3bf5-454a-ab89-bac3f1bd5864"
-
+pipeline {
     {
-        node("jenkins-slave"){
+        stages {
             // 第一步
-            stage('拉取代码'){
+            stage('拉取代码') {
                 git 'https://github.com/HLDBanana/eureka_server.git'
             }
             // 第二步
-            stage('代码编译'){
+            stage('代码编译') {
                 sh "mvn clean package -Dmaven.test.skip=true"
             }
             // 第三步
-            stage('构建镜像'){
+            stage('构建镜像') {
                 sh """
                     echo '
                         FROM 47.114.1.23/hld-k8s/centos-jdk11
@@ -45,3 +45,4 @@ def k8s_auth = "9914859b-3bf5-454a-ab89-bac3f1bd5864"
 //            }
         }
     }
+}
